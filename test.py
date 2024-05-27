34ba96163d90addce23bdd1014b3d01c8769b505
yashinshekh <developeryasin@gmail.com>
2024-05-27 23:17:07 +0545

    asf

--- test.py
+++ test.py
@@ -11,6 +11,7 @@ options.add_argument(r"user-data-dir=C:\\Users\\hanss\\AppData\\Local\\Google\\C
 options.add_argument("profile-directory=Profile 2")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-extensions")
 options.add_argument("--disable-gpu")
 
 # Initialize the Chrome driver with the specified options
