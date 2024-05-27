ea17439456cdf3d96c9da556524c47003a1a78e4
yashinshekh <developeryasin@gmail.com>
2024-05-27 23:14:03 +0545

    asf

--- test.py
+++ test.py
@@ -1,26 +1,21 @@
-import os
-import undetected_chromedriver as uc
+from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 # Create Chrome options
-# options = uc.ChromeOptions()
-
 options = webdriver.ChromeOptions()
-options.add_argument(r"--user-data-dir=C:\\Users\\hanss\\AppData\\Local\\Google\\Chrome\\User Data")
-options.add_argument("--profile-directory=Profile 2")
+options.add_argument(r"user-data-dir=C:\\Users\\hanss\\AppData\\Local\\Google\\Chrome\\User Data")
+options.add_argument("profile-directory=Profile 2")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--disable-gpu")
-options.add_argument("--remote-debugging-port=9222")  # Ensure remote debugging is enabled
 
 # Initialize the Chrome driver with the specified options
-service = Service(executable_path=os.getcwd()+'/chromedriver.exe')
-driver = webdriver.Chrome(options=options,service=service)
+service = ChromeService(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=options)
 
 try:
     # Open a website to test
